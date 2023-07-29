25c4064d002438ac22276a38c631f656ae9e504e
Geoff George <geoffgeorgethommana@gmail.com>
2023-07-29 16:13:22 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      
+      <main>
+        <header>
+          <a href="">My Blog</a>
+          <nav>
+            <a href="">Login</a>
+            <a href="">Register</a>
+          </nav>
+        </header>
+
+      <div className="post">
+        
+        <div className="Image">
+
+          <img src="https://techcrunch.com/wp-content/uploads/2022/09/GettyImages-1158587830-copy.jpg?w=1390&crop=1" alt="" />
+
+        </div>
+        
+        <div className="texts"> 
+
+          <h2> If it hadn’t been for them meddlin’ kids</h2>
+          <p> Welcome to Startups Weekly. Sign up here to get it in your inbox every Friday. Not to get all GrumpyManYellsAtCloud.gif, but I’m getting pretty tired of the myth of the dropped-out-of-college found...</p>
+        </div>
+        
+
+      </div>
+
+      <div className="post">
+
+        <div className="Image">
+
+          <img src="https://techcrunch.com/wp-content/uploads/2022/09/GettyImages-1158587830-copy.jpg?w=1390&crop=1" alt="" />
+
+        </div>
+
+        <div className="texts"> 
+
+          <h2> If it hadn’t been for them meddlin’ kids</h2>
+          <p> Welcome to Startups Weekly. Sign up here to get it in your inbox every Friday. Not to get all GrumpyManYellsAtCloud.gif, but I’m getting pretty tired of the myth of the dropped-out-of-college found...</p>
+        </div>
+
+        
+      </div>
+
+
+      <div className="post">
+
+        <div className="Image">
+
+          <img src="https://techcrunch.com/wp-content/uploads/2022/09/GettyImages-1158587830-copy.jpg?w=1390&crop=1" alt="" />
+
+        </div>
+
+        <div className="texts"> 
+
+          <h2> If it hadn’t been for them meddlin’ kids</h2>
+          <p> Welcome to Startups Weekly. Sign up here to get it in your inbox every Friday. Not to get all GrumpyManYellsAtCloud.gif, but I’m getting pretty tired of the myth of the dropped-out-of-college found...</p>
+        </div>
+      </div>
+
+
+
+      </main>
+
+    
   );
 }
 

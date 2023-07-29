74aa11936c0bc7d20a53a2a57c296d931ed2d00d
Geoff George <geoffgeorgethommana@gmail.com>
2023-07-29 20:25:11 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,62 +1,43 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Post from './post';
+import Header from './header';
 
 function App() {
   return (
     
-      
-      <main>
-        <header>
-          <a href="" className="logo">My Blog</a>
-          <nav>
-            <a href="">Login</a>
-            <a href="">Register</a>
-          </nav>
-        </header>
+      <Routes>
 
-        <Post/>
-        <Post/>
-        <Post/>
+        <Route
 
-      
+          index element={
 
-      <div className="post">
+              <main>
+              
+                <Header/>
 
-        <div className="Image">
+                <Post/>
+                <Post/>
+                <Post/>
 
-          <img src="https://techcrunch.com/wp-content/uploads/2022/09/GettyImages-1158587830-copy.jpg?w=1390&crop=1" alt="" />
 
-        </div>
+              </main>
+          }
 
-        <div className="texts"> 
 
-          <h2> If it hadn’t been for them meddlin’ kids</h2>
-          <p> Welcome to Startups Weekly. Sign up here to get it in your inbox every Friday. Not to get all GrumpyManYellsAtCloud.gif, but I’m getting pretty tired of the myth of the dropped-out-of-college found...</p>
-        </div>
+        />
 
+        <Route path={'/login'} element={
+
+            <div>
+              Login
+            </div>
+        }
+
+        />  
         
-      </div>
-
-
-      <div className="post">
-
-        <div className="Image">
-
-          <img src="https://techcrunch.com/wp-content/uploads/2022/09/GettyImages-1158587830-copy.jpg?w=1390&crop=1" alt="" />
-
-        </div>
-
-        <div className="texts"> 
-
-          <h2> If it hadn’t been for them meddlin’ kids</h2>
-          <p> Welcome to Startups Weekly. Sign up here to get it in your inbox every Friday. Not to get all GrumpyManYellsAtCloud.gif, but I’m getting pretty tired of the myth of the dropped-out-of-college found...</p>
-        </div>
-      </div>
-
-
-
-      </main>
+      </Routes>
 
     
   );
---
+++ src/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+            <a href="" className="logo">My Blog</a>
+            <nav>
+              <Link to="/login"> Login </Link>
+              <Link to="">Register</Link>
+            </nav>
+    </header>
+  )
+}
+
+export default Header;

80a6a985df67b907e1f0d7a173d184467d14d07d
Geoff George <geoffgeorgethommana@gmail.com>
2023-07-29 19:58:48 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Post from './post';
 
 function App() {
   return (
@@ -7,29 +8,18 @@ function App() {
       
       <main>
         <header>
-          <a href="">My Blog</a>
+          <a href="" className="logo">My Blog</a>
           <nav>
             <a href="">Login</a>
             <a href="">Register</a>
           </nav>
         </header>
 
-      <div className="post">
-        
-        <div className="Image">
+        <Post/>
+        <Post/>
+        <Post/>
 
-          <img src="https://techcrunch.com/wp-content/uploads/2022/09/GettyImages-1158587830-copy.jpg?w=1390&crop=1" alt="" />
-
-        </div>
-        
-        <div className="texts"> 
-
-          <h2> If it hadn’t been for them meddlin’ kids</h2>
-          <p> Welcome to Startups Weekly. Sign up here to get it in your inbox every Friday. Not to get all GrumpyManYellsAtCloud.gif, but I’m getting pretty tired of the myth of the dropped-out-of-college found...</p>
-        </div>
-        
-
-      </div>
+      
 
       <div className="post">
 
---
+++ src/post.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div className="post">
+        
+        <div className="Image">
+
+          <img src="https://techcrunch.com/wp-content/uploads/2022/09/GettyImages-1158587830-copy.jpg?w=1390&crop=1" alt="" />
+
+        </div>
+        
+        <div className="texts"> 
+
+          <h2> If it hadn’t been for them meddlin’ kids</h2>
+          <p> Welcome to Startups Weekly. Sign up here to get it in your inbox every Friday. Not to get all GrumpyManYellsAtCloud.gif, but I’m getting pretty tired of the myth of the dropped-out-of-college found...</p>
+        </div>
+        
+
+      </div>
+  )
+}
+
+export default Post
